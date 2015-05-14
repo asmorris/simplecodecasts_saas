6fadebfaefd6d601a2018c1f7ef72b005f672980
Andrew Morrison <morro141@gmail.com>
2015-05-14 03:06:08 +0000

    Add pages controller, home view and route

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  def title
+    @base_title = 'Dev Match'
+    
+    if @title.nil?
+      @base_title
+    else
+      "#{@title} | #{@base_title}"
+    end
+  end
 end
